73c5db2798fcc701e2314111828157740781615e
Bryce Bartlo <bbartlo1@pvlearners.net>
2022-10-23 16:21:59 -0700

    Update FullSetup.js

--- V3/FullSetup.js
+++ V3/FullSetup.js
@@ -6,28 +6,31 @@ export async function main(ns) {
 
     //Constants
 
-        const server = arr[0];
-        const targets = arr.slice(1);
-        
-	    const targeta = targets.length;
-		const targa = targeta*v3t;
+        const server = ns.args[0];
+        const targets = ns.args.slice(1);
+
+        const targeta = targets.length;
+
+        const ram_f1 = ns.getScriptRam("Full_V3.js") * targeta;
+        const ram_g1 = ns.getScriptRam("Grow_V3.js") * targeta;
+        const ram_w1 = ns.getScriptRam("Weaken_V3.js") * targeta;
+
+        const v3t = Math.floor(ns.getServerMaxRam(server) / ram_f1 + ram_g1 + ram_w1);
+        const targa = targeta* v3t;
 
         const ram_s = ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
         const ram_1 = ns.getScriptRam("Full_V3.js") * targa;
         const ram_2 = ns.getScriptRam("Grow_V3.js") * targa;
         const ram_3 = ns.getScriptRam("Weaken_V3.js") * targa;
         const ram_f = ram_s - ram_1 - ram_2 - ram_3;
-
-        const v3t = Math.floor(ns.getServerMaxRam(server) / ram_f1 + ram_g1 + ram_w1);
-            const ram_f1 = ns.getScriptRam("Full_V3.js") * targeta;
-            const ram_g1 = ns.getScriptRam("Grow_V3.js") * targeta;
-            const ram_w1 = ns.getScriptRam("Weaken_V3.js") * targeta; 
-
-
+    
         const ram_11 = ns.getScriptRam("Full_V3.js");   
         const ram_22 = ns.getScriptRam("Grow_V3.js");
         const ram_33 = ns.getScriptRam("Weaken_V3.js");
         const ram_ff = ram_11 + ram_22 + ram_33;
+		
+
+
 
     //Terminal prints (for information)
 
@@ -63,30 +66,4 @@ export async function main(ns) {
                 ns.print("Weaken script started to run on ", server, ". Target is ", target);
             }
         }
-
-    //Runs scripts fo all scan-analyze 1 servers
-
-        if (targets.find("A:")) {
-
-            const moreTargets = ["n00dles", "foodnstuff", "sigma-cosmetics", "joesguns", "hong-fang-tea", "harakiri-sushi", "iron-gym"]
-
-            ns.tprint("Targets are n00dles, foodnstuff, sigma-cosmetics, joesguns, hong-fang-tea, harakiri-sushi, iron-gym");
-
-            for (const target of moreTargets) {
-                ns.run("PortsNNuke.js", 1, target);
-      
-                ns.exec("Full_V3.js", server, v3t, target);
-                ns.print("Full (Hack, Grow, Weaken) script started to run on ", server, ". Target is ", target);
-                ns.exec("Grow_V3.js", server, v3t, target);
-                ns.print("Grow script started to run on ", server, ". Target is ", target);
-                ns.exec("Weaken_V3.js", server, v3t, target);
-                ns.print("Weaken script started to run on ", server, ". Target is ", target);
-            }
-
-            ns.tprint("New set of scripts running on server A: , targets are n00dles, foodnstuff, sigma-cosmetics, joesguns, hong-fang-tea, harakiri-sushi, iron-gym");
-    
-            await ns.sleep(10000);
-            ns.tprint("T- 10s Till script end")
-        }
 }
-    
