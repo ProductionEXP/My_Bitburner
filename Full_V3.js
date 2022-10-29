e42330a83d5adeff420edb0735886035517ef672
Bryce Bartlo <bbartlo1@pvlearners.net>
2022-10-28 21:05:09 -0700

    added all files outside of folder for easy script running

---
+++ Full_V3.js
@@ -0,0 +1,33 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const target = ns.args[0];
+    const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    const securityThresh = ns.getServerMinSecurityLevel(target) + 1;
+
+    ns.disableLog("ALL");
+    ns.enableLog("print");
+
+    while (true) {
+
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            ns.print("Weakening - Current Server Security Level is: ", ns.getServerSecurityLevel(target), ". Goal is: ", securityThresh);
+            let start = performance.now();
+            await ns.weaken(target);
+            ns.print('Weaken took ' + ns.tFormat(performance.now() - start) + ' to finish');
+        }
+
+        else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            ns.print("Growing - Current Server Money is: ", ns.getServerMoneyAvailable(target), ". Goal is: ", moneyThresh);
+            let start = performance.now();
+            await ns.grow(target);
+            ns.print('Grow took ' + ns.tFormat(performance.now() - start) + ' to finish');
+        }
+
+        else {
+            let start = performance.now();
+            ns.print("Hacking - Current Server Money is: ", ns.getServerMoneyAvailable(target), ". Current Server Security Level is: ", ns.getServerSecurityLevel(target));
+            await ns.hack(target);
+            ns.print('Hack took ' + ns.tFormat(performance.now() - start) + ' to finish');
+        }
+    }
+}
