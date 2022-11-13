c717650a78167a5ae1d96919db610e7468c2dc4e
Bryce Bartlo <bbartlo1@pvlearners.net>
2022-11-13 12:36:57 -0700

    Log Scripts
    Active log Done, Start script started.
    Version PRV1.04.0.2

--- V4/Info/Active Logs/Hacknet_Log.js
+++
@@ -1,4 +0,0 @@
-/** @param {NS} ns */
-export function HacknetM(ns) {
-
-}
--- V4/Info/Active Logs/Server_Log.js
+++
@@ -1,8 +0,0 @@
-/** @param {NS} ns */
-export function ServerM(ns) {
-    ns.nFormat(ns.getTotalScriptIncome()[0], "$0,0.00")
-}
-
-export function ServerE(ns) {
-    ns.nFormat(ns.getTotalScriptExpGain(), "0,0.00")
-}
