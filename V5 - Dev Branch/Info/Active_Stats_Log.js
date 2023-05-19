ac9d61668855f231ff3607a740212343ec7ee4b6
Bryce Bartlo <114547919+ProductionEXP@users.noreply.github.com>
2023-05-18 18:28:52 -0700

    Minor Updates

--- V5 - Dev Branch/Info/Active_Stats_Log.js
+++ V5 - Dev Branch/Info/Active_Stats_Log.js
@@ -6,8 +6,6 @@ export async function main(ns) {
   ns.tail();ns.disableLog("ALL");ns.clearLog();
   ns.print("Active log started")
 
-  const timeinstall = ns.getResetInfo().lastAugReset/1000;
-
   const green = "\u001b[38;5;040m";
   const orange = "\u001b[38;5;202m";
 
@@ -16,10 +14,7 @@ export async function main(ns) {
     ns.print(`${green}Number Of Avilible Ports is ${portsnumber(ns)}`);
     ns.print(`${green}`);
     ns.print(`${green}Server income ${ns.formatNumber(ns.getTotalScriptIncome()[0])}/s`);
-    ns.print(`${green}Average Hacknet income ${ns.formatNumber(ns.getMoneySources().sinceInstall.hacknet/timeinstall)}/s`);
     ns.print(`${orange}Server EXP income ${ns.formatNumber(ns.getTotalScriptExpGain())}/s`);
-    ns.print(`${green}`);
-    ns.print(`${green}Total Income ${ns.formatNumber(ns.getTotalScriptIncome()[0] + (ns.getMoneySources().sinceInstall.hacknet/timeinstall))}/s`);
     await ns.sleep(10);
   }
 }
